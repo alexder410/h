86583c2ec39f651b23ca1063d718cbf3f7d0dbf7
alexder410 <162833244+alexder410@users.noreply.github.com>
2024-04-04 15:09:42 +0300

    Оновити pxt.json, main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 input.onButtonPressed(Button.A, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.InBackground)
+    basic.pause(2000)
     basic.showNumber(input.compassHeading())
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
     input.calibrateCompass()
 })
